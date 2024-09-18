c16c99b4ee98caaeb800c4b3eefb3d672d488280
gOnEbOnE <proaielaeu@gmail.com>
2024-09-18 14:09:02 +0700

    tutor 3

--- main/models.py
+++ main/models.py
@@ -1,9 +1,11 @@
 import uuid  # tambahkan baris ini di paling atas
+from django.contrib.auth.models import User
 
 # Create your models here.
 from django.db import models
 
 class MoodEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%4_04qj$(f14_jjvy3sa1&g*moahxef+vc-54!h3=_s#k6g!zi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://christopher-matthew31-mentalhealthtracker.pbp.cs.ui.ac.id","https://christopher-matthew31-mentalhealthtracker.pbp.cs.ui.ac.id" ]
 
