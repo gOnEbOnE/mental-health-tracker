30b178f6fa1395cbc939ebb58a60a9767b2b64e9
gOnEbOnE <proaielaeu@gmail.com>
2024-11-13 13:12:12 +0700

    tutor django

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = ''
 
 USE_I18N = True
 
